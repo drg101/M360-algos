fd07c6733d096b7f73cba08e7821449a727580ff
Daniel Reynolds <danielreynolds101@gmail.com>
2021-03-09 09:52:35 -0700

    add fermatFactor

--- fermatFactor.py
+++ fermatFactor.py
@@ -4,7 +4,7 @@ def fermatFactor(n,iter=20):
     for y in range(1,iter+1):
         nSq = (n + y**2)**0.5
         if int(nSq) == nSq:
-           print(f"Found @y={y}, int(nSq)")
+           print(f"Found @y={y}, {int(nSq)}")
            f0 = int(nSq - y)
            f1 = int(nSq + y)
            fac = {}
@@ -14,7 +14,10 @@ def fermatFactor(n,iter=20):
            return fac
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: " + sys.argv[0] + " test")
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        print("Usage: " + sys.argv[0] + " n [iterations]")
         quit()
-    print(fermatFactor(int(sys.argv[1])))
+    if len(sys.argv) == 2:
+        print(fermatFactor(int(sys.argv[1])))
+    if len(sys.argv) == 3:
+        print(fermatFactor(int(sys.argv[1]),int(sys.argv[2])))
