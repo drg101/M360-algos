e959d9e7579fff253433584ba166e32a4a39c23d
Daniel Reynolds <danielreynolds101@gmail.com>
2021-03-29 15:45:53 -0600

    addition

--- zeroKnowledgeProof.py
+++ zeroKnowledgeProof.py
@@ -6,3 +6,9 @@ def zeroKnowledgeProof(y,b,z,n,x=None):
         return powmod(z,2,n) == y
     elif b == 1:
         return powmod(z,2,n) == (x * y) % n
+
+if __name__ == "__main__":
+    if len(sys.argv) != 6:
+            print("Usage: " + sys.argv[0] + " y b z n x")
+            quit()
+    print(zeroKnowledgeProof(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5])))
