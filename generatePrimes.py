835d195d4cbd88c21834f083778ca1c861dd558e
Daniel Reynolds <danielreynolds101@gmail.com>
2021-04-23 16:11:57 -0600

    added sieve

--- generatePrimes.py
+++ generatePrimes.py
@@ -2,13 +2,20 @@ import sys
 import isPrime
 
 def generatePrimes(n):
+    return sieve(n)
+
+def sieve(n):
+    tab = [x for x in range(2,n+1)]
+    safe = [0 for _ in range(2,n+1)]
+    for i in range(len(tab)):
+        if safe[i] == 0:
+            for multiple in range(i+tab[i],len(safe),tab[i]):
+                safe[multiple] = 1
     primes = []
-    for i in range(2, n + 1):
-        if isPrime.isPrime(i):
-            primes.append(i)
-
+    for i in range(len(safe)):
+        if safe[i] == 0:
+            primes.append(tab[i])
     return primes
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
